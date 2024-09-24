1316ffd7d79b90c8b66cea74cc4a16766d484029
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-09-24 06:52:01 -0300

    feat: implementing people repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,4 +1,5 @@
 import pytest
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 
@@ -16,3 +17,23 @@ def test_delet_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="db interaction")
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 28
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="db interaction")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+
+    print()
+    print(response)
