f5dacde52cdb3005abf19c05c0788edce728e184
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-10-03 10:32:58 -0300

    feat: implementing person finder route

---
+++ src/main/composer/person_finder_composer.py
@@ -0,0 +1,11 @@
+from src.views.person_finder_view import PersonFinderView
+from src.controllers.person_finder_controller import PersonFinderController
+from src.models.sqlite.repositories.people_repository import PeopleRepository
+from src.models.sqlite.settings.connection import db_connection_handler
+
+def person_finder_composer():
+    model = PeopleRepository(db_connection_handler)
+    controller = PersonFinderController(model)
+    view = PersonFinderView(controller)
+
+    return view
